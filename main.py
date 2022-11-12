1c07e942faefc8a67cb449b48a298b8802452633
Lizette Dimalanta <105244120+Lizette-Dimalanta@users.noreply.github.com>
2022-11-12 15:04:20 +1100

    Added User Schema

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import os
 
+db = SQLAlchemy()
+
 def create_app():
     app = Flask(__name__)
 
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 
-    db = SQLAlchemy(app)
+    db.init_app(app)
 
     @app.route('/')
     def index():
